e0d8e7e5d8f581c654312027fb0e2399cc188fd7
John Lester L. Licayan <johnlesterincbusiness@gmail.com>
2022-10-09 19:43:20 +0800

    Initial Commit

---
+++ app/src/main/java/com/himes/Items.java
@@ -0,0 +1,19 @@
+package com.himes;
+
+
+public class Items {
+    
+    private String itemName;
+    private String itemId;
+    private int itemCount;
+    private int itemNo;
+    
+    
+    public Item(String itemName, String itemId, int itemCount, int itemNo) {
+        this.itemName = itemName;
+        this.itemId = itemId;
+        this.itemCount = itemCount;
+        this.itemNo = itemNo;
+    }
+    
+}
